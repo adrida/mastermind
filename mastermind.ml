8dcec844f5682606f92cd465f47a25605464c613
Adam Rida <adam.rida@eisti.fr>
2019-01-12 16:44:01 +0100

    suppressions fonctions inutiles (duplicatas jouer() et nmenu () );

--- mastermind.ml
+++ mastermind.ml
@@ -66,73 +66,10 @@ aux (x + 1) (ListCouleur.elagage prop (bp,mp) l)
 	with Failure("int_of_string") ->print_string "Erreur de saisie!\n"; aux x l;
 	in aux 1 liste;;
 
-	(*
-	 let jouer  nbcoup liste auto listerep =
-	 let rec aux n l =
-	 match (n,l) with
-	 |_,[]-> raise Perdu
-	 |x,[h]->
-	 let prop =hd l in
-	 ListCouleur.print_list prop;
-	 print_string "est la bonne combinaison !\n";
-	 print_newline();
-	 |x,_ when x>nbcoup && nbcoup<>0->
-	 print_string "Plus d'essai: PERDU!\n";
-	 print_newline();
-	 |x,_->
-	 let prop=hd l in
-	 print_string "Essai ";
-	 print_int x;
-	 print_string " : ";
-	 ListCouleur.print_list prop;
-	 print_string "\n";
-	 if auto == false then
-	print_string "Nombre de pion(s) bien placé(s):\n";
-	try
-	let bp=read_int() in
-	print_string "Nombre de pions(s) mal placé(s):\n";
-	let mp=read_int() in
-	print_newline();
-aux (x + 1) (ListCouleur.elagage prop (bp,mp) l)
-	with Failure("int_of_string") ->print_string "Erreur de saisie!\n"; aux x l;
-	in aux 1 liste;
-	else 
-	print_string "Nombre de pion(s) bien placé(s):\n";
-	try
-	let bp= fst (indication listerep liste) in
-	print_string "Nombre de pions(s) mal placé(s):\n";
-	let mp= snd (indication listerep liste) in
-	print_newline();
-aux (x + 1) (ListCouleur.elagage prop (bp,mp) l)
-	with Failure("int_of_string") ->print_string "Erreur de saisie!\n"; aux x l;
-	in aux 1 liste;;
-	*)
+
 	(* Menu récursif pour rejouer *)
 	(* gestion de la Perte*)
-(* choix du mode de jeu *)
-	(*
-	 let rec menu2 listC listSR=
-	 print_newline();
-	 print_string "Choisissez votre combinaison de 5 couleurs parmis les couleurs suivantes and keep it in your mind :):\n -Rouge|Bleu|Vert|Noir|Jaune|Orange|Violet|Blanc-\n";
-	 try
-	 print_string "Choisissez votre mode de jeu (1: Sans redondance, 2: Avec redondance, 3: avec nombre de coup limité, 0: quitter)\n";
-	 let mode=read_int() in
-	 match  mode with
-	 |1->jouer 0 listSR auto ;
-	 menu listC listSR    
-	 |2->jouer 0 listC auto ;
-	 menu listC listSR   
-	 |3->print_string "Combien de coups maximum souhaitez vous?\n";
-	 let nbcoup=read_int() in        
-	 jouer nbcoup listC auto ;
-	 menu listC listSR      
-	 |0->print_string "Merci d'avoir joué!\n"
-	 |_-> print_string "Erreur de choix\n";
-	 menu listC listSR 
-	 with 
-	 |Failure("int_of_string")-> print_string "Erreur de saisie!\n"; menu listC listSR
-	|Perdu -> print_string "Perdu! C'est pas bien\n"; menu listC listSR;;
-	*)
+
 	let rec menu2 nom_joueur coup_max nb_parties auto listeSR listeComplete listeRP= 
 	print_newline();
 	print_string ">>  Joueur :  ";
