fe8066735ebea854f69a012383970b9349decc1a
Adam Rida <ridaadam@eisti.fr>
2019-01-18 20:40:59 +0100

    rm deux fonctions inutiles en commentaire dans tools.ml

--- tools.ml
+++ tools.ml
@@ -123,25 +123,4 @@ aux t (h::res)
 	|Blanc->  print_string "Blanc ";
 	print_list t ;;
 
-	(*
-
-	 let string_of_code c = 
-	 match c with
-	 | [] -> ()
-	 | h::t -> match h with
-	 |Rouge->  "Rouge ";
-	 |Bleu ->  "Bleu ";
-	 |Vert-> "Vert ";
-	 |Noir-> "Noir ";
-	 |Jaune->  "Jaune ";
-	 |Orange-> "Orange ";
-	 |Violet-> "Violet ";
-	 |Blanc-> "Blanc ";
-
-	 let code_of_string liste_Pion =
-	 let rec liste_Pion_to_string_aux liste_Pion s=
-	 match liste_Pion with
-	 |[] -> "" 
-	 |h::t -> (string_of_code h)^(liste_Pion_to_string_aux t s)
-	 in liste_Pion_to_string_aux liste_Pion "";;*)
 	end ;;
